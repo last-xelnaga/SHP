50b81ecc2504a1e13ba1f2b4a086a879b0391966
Vadims Tihomirovs <vadims.tihomirovs@sonymobile.com>
2016-03-08 16:59:58 +0100

    makefile is updated to use libconfig

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,26 @@
+
 #include <stdio.h>
+#include "../external/libconfig/libconfig.h"
+
 
 int main (
-    void)
+    int argc,
+    char **argv)
 {
-    printf("OK\n");
+    config_t cfg;
+    config_init(&cfg);
+
+    // Read the file. If there is an error, report it and exit.
+    if (!config_read_file(&cfg, "shp_client.cfg"))
+    {
+        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),
+                config_error_line(&cfg), config_error_text(&cfg));
+        config_destroy(&cfg);
+        return -1;
+    }
+
+
+
+    config_destroy(&cfg);
+    return 0;
 }
