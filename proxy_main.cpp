c649fb547dacaaa34391c5ebb9465e3b91b1efba
Vadims Tihomirovs <vadims.tihomirovs@sonymobile.com>
2016-05-16 17:41:45 +0200

    force proxy to listen and show messages

--- proxy_main.cpp
+++ proxy_main.cpp
@@ -1,10 +1,5 @@
-//#include <stdio.h>
-//#include <unistd.h>
-//#include <signal.h>
 
-//#include "config.hpp"
-//#include "sensor_manager.hpp"
-//#include "queue_manager.hpp"
+#include "message.hpp"
 #include "debug.hpp"
 #include "socket.hpp"
 
@@ -16,17 +11,50 @@ int main (
     DEBUG_LOG_TRACE_BEGIN
 
     unsigned char p_buffer [128];
-    //unsigned int message_length = 0;
+    unsigned char* p_header_ptr = p_buffer;
+    unsigned char* p_message_ptr = &p_buffer [sizeof (message_header_t)];
 
     server_socket_class server_socket;
-    server_socket.create_fd ();
+    result = server_socket.create_fd ();
 
-    while (1)
+    while (result == RESULT_OK)
     {
-        server_socket.accept_client ();
-        server_socket.recv_data (p_buffer, 1);
-        //message_length = p_buffer[0];
-        //server_socket.recv_data (&p_buffer[1], message_length);
+        //message_header_t header;
+
+        result = server_socket.accept_client ();
+
+        if (result == RESULT_OK)
+        {
+            result = server_socket.recv_data (p_header_ptr, sizeof (message_header_t));
+            if (result == RESULT_TRANSPORT_ERROR)
+            {
+                DEBUG_LOG_MESSAGE ("socket transport error. reset the existing connection");
+                server_socket.close_client ();
+                result = RESULT_OK;
+                continue;
+            }
+        }
+
+        if (result == RESULT_OK)
+        {
+            message_header_t* p_header = (message_header_t*) p_header_ptr;
+
+            result = server_socket.recv_data (p_message_ptr, p_header->payload_size);
+            if (result == RESULT_TRANSPORT_ERROR)
+            {
+                DEBUG_LOG_MESSAGE ("socket transport error. reset the existing connection");
+                server_socket.close_client ();
+                result = RESULT_OK;
+                continue;
+            }
+        }
+
+        if (result == RESULT_OK)
+        {
+            message_header_t* p_header = (message_header_t*) p_header_ptr;
+            unsigned int message_length = sizeof (message_header_t) + p_header->payload_size;
+            DEBUG_LOG_PRINT_ARRAY ("message", p_buffer, message_length);
+        }
     }
 
     DEBUG_LOG_TRACE_END (result)
