452928e7caa5fcaa8307de07cfb07c48e00e7400
Vadims Tihomirovs <vadims.tihomirovs@sonymobile.com>
2016-05-16 17:46:20 +0200

    queue_manager updated with simple logic

--- queue_manager.cpp
+++ queue_manager.cpp
@@ -1,35 +1,163 @@
 
-//#include <stdlib.h>
-//#include <unistd.h>
-//#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <map>
 
 #include "queue_manager.hpp"
 #include "message.hpp"
 #include "socket.hpp"
+#include "debug.hpp"
 
+typedef std::map <unsigned int, message_class*> message_map_t;
+static message_map_t message_map;
+static unsigned int message_count;
 
-queue_manager_class *queue_manager_class::p_instance = 0;
+static pthread_mutex_t map_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+queue_manager_class* queue_manager_class::p_instance = 0;
 
 queue_manager_class::queue_manager_class (
         void)
 {
-
+    p_socket = 0;
+    message_count = 0;
 }
 
-void queue_manager_class::set_socket (
-        socket_class* p_socket)
-{
-
-}
-
-int queue_manager_class::add_message (
+error_code_t queue_manager_class::send_message (
         message_class* p_message)
 {
-    return 0;
+    error_code_t result = RESULT_OK;
+    DEBUG_LOG_TRACE_BEGIN
+
+    if (result == RESULT_OK)
+    {
+        result = p_socket->create_fd ();
+    }
+
+    if (result == RESULT_OK)
+    {
+        unsigned char* p_data = 0;
+        unsigned int data_length = 0;
+        p_message->get_header (&p_data, &data_length);
+        result = p_socket->send_data (p_data, data_length);
+    }
+
+    if (result == RESULT_OK)
+    {
+        unsigned char* p_data = 0;
+        unsigned int data_length = 0;
+        p_message->get_payload (&p_data, &data_length);
+        result = p_socket->send_data (p_data, data_length);
+    }
+
+    p_socket->close_fd ();
+
+    DEBUG_LOG_TRACE_END (result)
+    return result;
 }
 
-int queue_manager_class::run (
-        void)
+error_code_t queue_manager_class::peek_message (
+        unsigned int* message_id,
+        message_class** p_message)
 {
-    return 0;
+    error_code_t result = RESULT_OK;
+    DEBUG_LOG_TRACE_BEGIN
+
+    // enter the critical section, lock the mutex
+    pthread_mutex_lock (&map_mutex);
+
+    if (message_map.size ())
+    {
+        message_map_t::iterator it = message_map.begin ();
+        *message_id = it->first;
+        *p_message = it->second;
+    }
+    else
+    {
+        *message_id = 0;
+    }
+
+    // we finished with critical sectio, so unlock the mutex
+    pthread_mutex_unlock (&map_mutex);
+
+    DEBUG_LOG_TRACE_END (result)
+    return result;
+}
+
+error_code_t queue_manager_class::remove_message (
+        unsigned int message_id)
+{
+    error_code_t result = RESULT_OK;
+    DEBUG_LOG_TRACE_BEGIN
+
+    // enter the critical section, lock the mutex
+    pthread_mutex_lock (&map_mutex);
+
+    message_map_t::iterator it = message_map.find (message_id);
+    if (it != message_map.end ())
+    {
+        delete (it->second);
+        message_map.erase (it);
+    }
+
+    // we finished with critical sectio, so unlock the mutex
+    pthread_mutex_unlock (&map_mutex);
+
+    DEBUG_LOG_TRACE_END (result)
+    return result;
+}
+
+error_code_t queue_manager_class::add_message (
+        message_class* p_message)
+{
+    error_code_t result = RESULT_OK;
+    DEBUG_LOG_TRACE_BEGIN
+
+    // enter the critical section, lock the mutex
+    pthread_mutex_lock (&map_mutex);
+
+    message_map.insert (std::pair <int, message_class*> (++ message_count, p_message));
+
+    // we finished with critical sectio, so unlock the mutex
+    pthread_mutex_unlock (&map_mutex);
+
+    DEBUG_LOG_TRACE_END (result)
+    return result;
+}
+
+error_code_t queue_manager_class::run (
+        client_socket_class* p_client_socket)
+{
+    error_code_t result = RESULT_OK;
+    DEBUG_LOG_TRACE_BEGIN
+
+    p_socket = p_client_socket;
+
+    while (result == RESULT_OK)
+    {
+        unsigned int message_id = 0;
+        message_class* p_message = 0;
+
+        sleep (2);
+        //usleep (500000);
+
+        result = peek_message (&message_id, &p_message);
+        if (message_id == 0)
+        {
+            continue;
+        }
+
+        if (result == RESULT_OK)
+        {
+            result = send_message (p_message);
+        }
+
+        if (result == RESULT_OK)
+        {
+            remove_message (message_id);
+        }
+    }
+
+    DEBUG_LOG_TRACE_END (result)
+    return result;
 }
--- queue_manager.hpp
+++ queue_manager.hpp
@@ -2,9 +2,10 @@
 #ifndef QUEUE_MANAGER_HPP
 #define QUEUE_MANAGER_HPP
 
-//#include "sensors/sensor.hpp"
+#include "error_codes.hpp"
 
-class socket_class;
+
+class client_socket_class;
 class message_class;
 
 class queue_manager_class
@@ -13,11 +14,25 @@ private:
     static queue_manager_class* p_instance;
 
     // list of messages
+
+    //tee_handle_map_t tee_handle_map;
+
     // socket
+    client_socket_class* p_socket;
 
 private:
     queue_manager_class (
             void);
+
+    error_code_t send_message (
+            message_class* p_message);
+
+    error_code_t peek_message (
+            unsigned int* message_id,
+            message_class** p_message);
+
+    error_code_t remove_message (
+            unsigned int message_id);
 
 public:
 
@@ -25,18 +40,15 @@ public:
         void)
     {
         if (!p_instance)
-            p_instance = new queue_manager_class();
+            p_instance = new queue_manager_class ();
         return p_instance;
     }
 
-    void set_socket (
-            socket_class* p_socket);
-
-    int add_message (
+    error_code_t add_message (
             message_class* p_message);
 
-    int run (
-            void);
+    error_code_t run (
+            client_socket_class* p_client_socket);
 
 };
 
