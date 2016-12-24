54010e8856d2c2d719848129ca9dc1e615ff2530
Vadims Tihomirovs <asiris.mail@gmail.com>
2016-12-24 10:42:30 +0100

    allow user to setup cross compilation via setup.sh
    setup.sh does not wait for argument anymore. the script can
    identify the x86_64 architecture and ask user about cross-compile
    tool.

--- setup.sh
+++ setup.sh
@@ -2,87 +2,94 @@
 
 function info()
 {
-	echo -e "\e[00;32m$*\e[00m" >&2 #green
+    echo -e "\e[00;32m$*\e[00m" >&2 #green
 }
 
 function warn()
 {
-	echo -e "\e[00;35m$*\e[00m" >&2 #purple
+    echo -e "\e[00;35m$*\e[00m" >&2 #purple
 }
 
 function error()
 {
-	echo -e "\e[00;31m$*\e[00m" >&2 #green
-	exit 1
+    echo -e "\e[00;31m$*\e[00m" >&2 #green
+    exit 1
 }
 
 # workspace setup
 root=$(pwd)
 tools=$root/../tools
-target=""
 
-if [ -z "$1" ]; then
-	echo "no argument supplied, so build for arm"
-else
-	echo "argument supplied, so build for pc"
-	target="pc"
+
+# check the machine architecture
+arch=$(uname --machine)
+if [ $arch == "x86_64" ]; then
+    info "you are using x86 ($arch) architecture"
+    info "do you want to setup the cross compiler?"
+    read -p "[y/n]" -n 1 -r
+    echo    # new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # get cross-compile tools
+        if [ ! -d $tools ]; then
+            cd $root/..
+
+            info "clone tools..."
+            git clone https://github.com/raspberrypi/tools
+            if [ "$?" -ne "0" ]; then
+                error "failed to get tools"
+            fi
+
+            info "checkout old but stable version..."
+            cd $tools
+            git checkout 3a413ca2b23fd275e8ddcc34f3f9fc3a4dbc723f
+            if [ "$?" -ne "0" ]; then
+                error "failed to checkout"
+            fi
+        else
+            info "tools already in place, skipping..."
+        fi
+
+        info "create makefile.prefix ..."
+        cd $root
+        echo "PREFIX = $tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-" > makefile.prefix
+    else
+        echo "PREFIX = " > makefile.prefix
+    fi
 fi
 
-# get cross-compile tools
-if [ -z "$target" ]; then
-	if [ ! -d $tools ]; then
-		cd $root/..
-
-		info "clone tools..."
-		git clone https://github.com/raspberrypi/tools
-		if [ "$?" -ne "0" ]; then
-			error "failed to get tools"
-		fi
-
-		info "checkout old but stable version..."
-		cd $tools
-		git checkout 3a413ca2b23fd275e8ddcc34f3f9fc3a4dbc723f
-		if [ "$?" -ne "0" ]; then
-			error "failed to checkout"
-		fi
-	else
-		info "tools already in place, skipping..."
-	fi
-
-	info "create makefile.prefix ..."
-	cd $root
-	#if [ ! -f makefile.prefix ]; then
-	 	echo "PREFIX	= $tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-" > makefile.prefix
-	#fi
-else
-	echo "PREFIX	= " > makefile.prefix
-fi
-
-
+echo
 info "process gtest ..."
 cd external/gtest
 make clean
 make
 cd $root
 
+echo
 info "process libconfig ..."
 cd external/libconfig
 make clean
 make
 cd $root
 
+echo
 info "process wiringPi ..."
 cd external/wiringPi/wiringPi
 make clean
 make static
 cd $root
 
+echo
 info "process zlib ..."
 cd external/zlib
 make clean
 make static
+cd zpipe
+make clean
+make all
+cp zpipe $root/
 cd $root
 
+echo
 info "process shp ..."
 make clean
 make
